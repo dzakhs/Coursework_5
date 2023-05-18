38389d7244a7c1bf41366c1d9c6c9695c8b388a7
Dmitry Zakhs <d.zakhs@gmail.com>
2023-05-19 01:46:32 +0300

    fix errors

--- sql/create_db.py
+++ sql/create_db.py
@@ -6,7 +6,7 @@ def create_db_and_tables(db_name, params):
     conn.autocommit = True
     cur = conn.cursor()
 
-
+    cur.execute(f"DROP DATABASE {db_name}")
     cur.execute(f"CREATE DATABASE {db_name}")
 
     conn.close()
@@ -16,7 +16,7 @@ def create_db_and_tables(db_name, params):
     with conn.cursor() as cur:
         cur.execute("""
                 CREATE TABLE employeers (
-                    emp_id int PRIMARY KEY,
+                    emp_id INTEGER NOT NULL,
                     name varchar(100) NOT NULL,
                     emp_url varchar (255) NOT NULL
                 )
@@ -25,11 +25,12 @@ def create_db_and_tables(db_name, params):
     with conn.cursor() as cur:
         cur.execute("""
                 CREATE TABLE vacancies (
-                    emp_id int REFERENCES employeers(emp_id),
+                    vac_id SERIAL PRIMARY KEY,
+                    emp_id INTEGER NOT NULL,
                     name varchar(255) NOT NULL,
                     url varchar(255) NOT NULL,
-                    salary_from int NOT NULL,
-                    salary_to int NOT NULL,
+                    salary_from INTEGER NOT NULL,
+                    salary_to INTEGER NOT NULL,
                     experience varchar(150),
                     description text
                 )
@@ -44,8 +45,17 @@ def insert_data(db_name, data, params):
 
     with conn.cursor() as cur:
         for vacancy in data:
+
+            cur.execute("""
+                         INSERT INTO employeers(emp_id, name, emp_url)
+                         VALUES (%s, %s, %s)
+                         """,
+                        (vacancy['employer']['id'], vacancy['employer']['name'], vacancy['employer']['alternate_url']))
+
+        for vacancy in data:
             salary_from = vacancy['salary']['from'] if vacancy['salary']['from'] is not None else 0
             salary_to = vacancy['salary']['from'] if vacancy['salary']['from'] is not None else 0
+
             cur.execute("""
              INSERT INTO vacancies(emp_id, name, url, salary_from, salary_to, experience, description)
              VALUES (%s, %s, %s, %s, %s, %s, %s)
@@ -53,11 +63,6 @@ def insert_data(db_name, data, params):
                         (vacancy['employer']['id'], vacancy['name'], vacancy['alternate_url'], salary_from, salary_to,
                          vacancy['experience']['name'], vacancy['snippet']['responsibility']))
 
-            cur.execute("""
-             INSERT INTO employeers(emp_id, name, emp_url)
-             VALUES (%s, %s, %s)
-             """,
-                        (vacancy['employer']['id'], vacancy['employer']['name'], vacancy['employer']['alternate_url']))
 
         conn.commit()
         conn.close()
--- src/hh_api_class.py
+++ src/hh_api_class.py
@@ -1,33 +1,36 @@
+import json
+from src.company_id import companies_id
 import requests
 
 class HeadHunterAPI:
     def __init__(self):
         self.url = "https://api.hh.ru/vacancies"
 
-    def get_vacancies(self,data):
+    def get_vacancies(self,emp_id):
         """
             Метод получения данных о вакансиях с сайта hh.ru по id компании
             param emp_id: id компании для поиска вакансий
             return: список со словарями
         """
-        for emp_id in data.values():
-            pages = 10
-            params = {
-                "employer_id" : emp_id,
-                "per_page" : 100,
-                "area" : 113,
-                "only_with_salary" : True
 
-            }
+        pages = 10
+        params = {
+            "employer_id" : emp_id,
+            "per_page" : 100,
+            "area" : 113,
+            "only_with_salary" : True
 
-            data = []
+        }
 
-            for page in range(pages):
+        data = []
 
-                responce = requests.get(self.url, params=params).json()['items']
-                data.extend(responce)
+        for page in range(pages):
+
+            responce = requests.get(self.url, params=params).json()['items']
+            data.extend(responce)
 
         return data
+
 
 
 
